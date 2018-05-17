ec575041034a2c24244e6125b2354dd9faf17c64
jim@moverve.com <jim@moverve.com>
2018-05-17 17:09:47 +0100

    Run tests without TeProc.

--- RunTest.cmd
+++ RunTest.cmd
@@ -1,5 +1,5 @@
-SETLOCAL ENABLEEXTENSIONS
+@SETLOCAL ENABLEEXTENSIONS
 @ECHO OFF
 SET wd=%programfiles(x86)%\Infuse Consulting\useMango\App
 ECHO Running test '%3' from project %2 on server %1
-"%wd%\TeProc.exe" -s %1 -p %2 --testname %3 | "%wd%\MangoMotor.exe"
+"%wd%\MangoMotor.exe" -s %1 -p %2 --testname %3 
